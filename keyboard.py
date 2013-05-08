3687b1125bb8f8f0866ac66403a275fd0ce47d2c
BoppreH <boppreh@gmail.com>
2013-05-07 21:42:03 -0300

    Minor refactoring

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,4 @@
 from collections import namedtuple
-import atexit
 
 KeyboardEvent = namedtuple('KeyboardEvent', ['event_type', 'key_code',
                                              'scan_code', 'alt_pressed',
@@ -9,11 +8,11 @@ handlers = []
 
 def listen():
     """
-    Calls `handler` for each keyboard event received. This is a blocking call.
+    Calls `handlers` for each keyboard event received. This is a blocking call.
     """
     # Adapted from http://www.hackerthreads.org/Topic-42395
     from ctypes import windll, CFUNCTYPE, POINTER, c_int, c_void_p, byref
-    import win32con, win32api, win32gui
+    import win32con, win32api, win32gui, atexit
 
     event_types = {win32con.WM_KEYDOWN: 'key down',
                    win32con.WM_KEYUP: 'key up',
@@ -23,7 +22,7 @@ def listen():
 
     def low_level_handler(nCode, wParam, lParam):
         """
-        Processes a low level window keyboard event.
+        Processes a low level Windows keyboard event.
         """
         event = KeyboardEvent(event_types[wParam], lParam[0], lParam[1],
                               lParam[2] == 32, lParam[3])
@@ -31,7 +30,7 @@ def listen():
             handler(event)
 
         # Be a good neighbor and call the next hook.
-        return windll.user32.CallNextHookEx(kbHook, nCode, wParam, lParam)
+        return windll.user32.CallNextHookEx(hook_id, nCode, wParam, lParam)
        
     # Our low level handler signature.
     CMPFUNC = CFUNCTYPE(c_int, c_int, c_int, POINTER(c_void_p))
@@ -39,19 +38,21 @@ def listen():
     pointer = CMPFUNC(low_level_handler)
 
     # Hook both key up and key down events for common keys (non-system).
-    kbHook = windll.user32.SetWindowsHookExA(win32con.WH_KEYBOARD_LL, pointer,
+    hook_id = windll.user32.SetWindowsHookExA(win32con.WH_KEYBOARD_LL, pointer,
                                              win32api.GetModuleHandle(None), 0)
 
     # Register to remove the hook when the interpreter exits. Unfortunately a
     # try/finally block doesn't seem to work here.
-    atexit.register(windll.user32.UnhookWindowsHookEx, kbHook)
+    atexit.register(windll.user32.UnhookWindowsHookEx, hook_id)
 
     while True:
         msg = win32gui.GetMessage(None, 0, 0)
         win32gui.TranslateMessage(byref(msg))
         win32gui.DispatchMessage(byref(msg))
 
-def print_event(e):
-    print e
-handlers.append(print_event)
-listen()
+if __name__ == '__main__':
+    def print_event(e):
+        print e
+
+    handlers.append(print_event)
+    listen()
