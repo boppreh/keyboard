1e86291054542e9006ee946712799ebc75240f2e
BoppreH <boppreh@gmail.com>
2013-05-07 21:32:29 -0300

    Remove SYSKEY* hooks and enable multiple handlers

--- keyboard.py
+++ keyboard.py
@@ -5,7 +5,9 @@ KeyboardEvent = namedtuple('KeyboardEvent', ['event_type', 'key_code',
                                              'scan_code', 'alt_pressed',
                                              'time'])
 
-def listen(handler):
+handlers = []
+
+def listen():
     """
     Calls `handler` for each keyboard event received. This is a blocking call.
     """
@@ -13,22 +15,22 @@ def listen(handler):
     from ctypes import windll, CFUNCTYPE, POINTER, c_int, c_void_p, byref
     import win32con, win32api, win32gui
 
-    # Used for receiving ALT key presses.
-    WM_SYSKEYDOWN = 0x0104
-    WM_SYSKEYUP = 0x0105
+    event_types = {win32con.WM_KEYDOWN: 'key down',
+                   win32con.WM_KEYUP: 'key up',
+                   0x104: 'key down', # WM_SYSKEYDOWN, used for Alt key.
+                   0x105: 'key up', # WM_SYSKEYUP, used for Alt key.
+                  }
 
     def low_level_handler(nCode, wParam, lParam):
         """
         Processes a low level window keyboard event.
         """
-        if wParam in [win32con.WM_KEYDOWN, WM_SYSKEYDOWN]:
-            event_type = 'key down'
-        if wParam in [win32con.WM_KEYUP, WM_SYSKEYUP]:
-            event_type = 'key up'
-
-        event = KeyboardEvent(event_type, lParam[0], lParam[1],
+        event = KeyboardEvent(event_types[wParam], lParam[0], lParam[1],
                               lParam[2] == 32, lParam[3])
-        handler(event)
+        for handler in handlers:
+            handler(event)
+
+        # Be a good neighbor and call the next hook.
         return windll.user32.CallNextHookEx(kbHook, nCode, wParam, lParam)
        
     # Our low level handler signature.
@@ -39,16 +41,11 @@ def listen(handler):
     # Hook both key up and key down events for common keys (non-system).
     kbHook = windll.user32.SetWindowsHookExA(win32con.WH_KEYBOARD_LL, pointer,
                                              win32api.GetModuleHandle(None), 0)
-    # Alt keypresses are not logged by KEYBOARD_LL, so we need SYSKEY* too.
-    kbHook = windll.user32.SetWindowsHookExA(WM_SYSKEYDOWN, pointer,
-                                             win32api.GetModuleHandle(None), 0)
-    # And now do the same for the release events.
-    kbHook = windll.user32.SetWindowsHookExA(WM_SYSKEYUP, pointer,
-                                             win32api.GetModuleHandle(None), 0)
 
     # Register to remove the hook when the interpreter exits. Unfortunately a
     # try/finally block doesn't seem to work here.
     atexit.register(windll.user32.UnhookWindowsHookEx, kbHook)
+
     while True:
         msg = win32gui.GetMessage(None, 0, 0)
         win32gui.TranslateMessage(byref(msg))
@@ -56,4 +53,5 @@ def listen(handler):
 
 def print_event(e):
     print e
-listen(print_event)
+handlers.append(print_event)
+listen()
