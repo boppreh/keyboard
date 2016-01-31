755d48ab100ce1d30a92527f3aee8f0180e1ad38
BoppreH <boppreh@gmail.com>
2016-01-31 03:00:27 -0200

    Add write test

--- keyboard_tests.py
+++ keyboard_tests.py
@@ -2,9 +2,11 @@ import time
 import unittest
 import keyboard
 from keyboard_event import KeyboardEvent, canonical_names, KEY_DOWN, KEY_UP
+import string
 
 # Fake events with fake scan codes for a totally deterministic test.
-scan_codes_by_name = {name: i for i, name in enumerate(canonical_names.values())}
+all_names = list(canonical_names.values()) + list(string.ascii_lowercase) + ['shift']
+scan_codes_by_name = {name: i for i, name in enumerate(all_names)}
 class FakeEvent(KeyboardEvent):
 	def __init__(self, event_type, name):
 		self.event_type = event_type
@@ -12,10 +14,26 @@ class FakeEvent(KeyboardEvent):
 		self.scan_code = scan_codes_by_name[name]
 		self.time = time.time()
 
+class FakeOsKeyboard(object):
+	def __init__(self, append):
+		self.append = append
+		self.scan_code_table = {code: [(name, False)] for name, code in scan_codes_by_name.items()}
+
+	def press(self, scan_code):
+		self.append((KEY_DOWN, next(name for name, i in scan_codes_by_name.items() if i == scan_code)))
+
+	def release(self, scan_code):
+		self.append((KEY_UP, next(name for name, i in scan_codes_by_name.items() if i == scan_code)))
+
+	def map_char(self, char):
+		return scan_codes_by_name[char.lower()], char.isupper()
+
 class TestKeyboard(unittest.TestCase):
 	def setUp(self):
 		# We will use our own events, thank you very much.
 		keyboard.listener.listening = True
+		self.events = []
+		keyboard.os_keyboard = FakeOsKeyboard(self.events.append)
 
 	def press(self, name):
 		keyboard.listener.callback(FakeEvent(KEY_DOWN, name))
@@ -26,6 +44,11 @@ class TestKeyboard(unittest.TestCase):
 	def click(self, name):
 		self.press(name)
 		self.release(name)
+
+	def flush_events(self):
+		events = list(self.events)
+		self.events.clear()
+		return events
 
 	def test_is_pressed(self):
 		self.assertFalse(keyboard.is_pressed('enter'))
@@ -58,14 +81,25 @@ class TestKeyboard(unittest.TestCase):
 		return self.triggered
 
 	def test_register_hotkey(self):
-		self.assertFalse(self.triggers('space', [['enter']]))
-		self.assertTrue(self.triggers('space', [['space']]))
-		self.assertTrue(self.triggers('space, enter', [['space'], ['enter']]))
-		self.assertFalse(self.triggers('enter, space', [['space'], ['enter']]))
-		self.assertTrue(self.triggers('ctrl+space, enter', [['ctrl', 'space'], ['enter']]))
-		self.assertFalse(self.triggers('ctrl+space, enter', [['ctrl'], ['space'], ['enter']]))
-		self.assertTrue(self.triggers('ctrl+space, enter', [['space', 'ctrl'], ['enter']]))
-		self.assertTrue(self.triggers('ctrl+space, enter, space', [['ctrl', 'space'], ['enter'], ['ctrl', 'space'], ['enter'], ['space']]))
+		self.assertFalse(self.triggers('a', [['b']]))
+		self.assertTrue(self.triggers('a', [['a']]))
+		self.assertTrue(self.triggers('a, b', [['a'], ['b']]))
+		self.assertFalse(self.triggers('b, a', [['a'], ['b']]))
+		self.assertTrue(self.triggers('a+b', [['a', 'b']]))
+		self.assertTrue(self.triggers('ctrl+a, b', [['ctrl', 'a'], ['b']]))
+		self.assertFalse(self.triggers('ctrl+a, b', [['ctrl'], ['a'], ['b']]))
+		self.assertTrue(self.triggers('ctrl+a, b', [['a', 'ctrl'], ['b']]))
+		self.assertTrue(self.triggers('ctrl+a, b, a', [['ctrl', 'a'], ['b'], ['ctrl', 'a'], ['b'], ['a']]))
+
+	def test_write(self):
+		keyboard.write('a')
+		self.assertEqual(self.flush_events(), [(KEY_DOWN, 'a'), (KEY_UP, 'a')])
+
+		keyboard.write('ab')
+		self.assertEqual(self.flush_events(), [(KEY_DOWN, 'a'), (KEY_UP, 'a'), (KEY_DOWN, 'b'), (KEY_UP, 'b')])
+
+		keyboard.write('Ab')
+		self.assertEqual(self.flush_events(), [(KEY_DOWN, 'shift'), (KEY_DOWN, 'a'), (KEY_UP, 'a'), (KEY_UP, 'shift'), (KEY_DOWN, 'b'), (KEY_UP, 'b')])
 		
 
 
