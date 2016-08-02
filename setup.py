6b9c6b28a79eec4629b4fc13ab5697338cbdc5b6
BoppreH <boppreh@gmail.com>
2016-08-02 14:39:00 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 
 setup(
     name='keyboard',
-    version='0.7.0',
+    version='0.7.1',
     author='BoppreH',
     author_email='boppreh@gmail.com',
     packages=['keyboard'],
