a585adc7dc773a6d716f67c46ab0ba15b042734f
BoppreH <boppreh@gmail.com>
2016-10-23 13:31:21 +0200

    Remove testing features from __main__

--- keyboard/__main__.py
+++ keyboard/__main__.py
@@ -1,6 +1,3 @@
 import keyboard
-keyboard.wait('ctrl')
-exit()
 print('Press space twice to replay keyboard actions.')
-while True:
-    keyboard.play(keyboard.record('space, space'), 3)
+keyboard.play(keyboard.record('space, space'), 3)
