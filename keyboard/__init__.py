8cf8ba1216ced464d720e60b3f02f11d8a9f8354
BoppreH <boppreh@gmail.com>
2018-03-26 21:16:22 +0200

    Revert import changes to fix #133

--- keyboard/__init__.py
+++ keyboard/__init__.py
@@ -116,15 +116,14 @@ class _Event(_UninterruptibleEvent):
                 break
 
 import platform as _platform
-import importlib as _importlib
-_os_keyboard = _importlib.import_module(
-    {
-        'Windows': '._winkeyboard',
-        'Linux': '._nixkeyboard',
-        'Darwin': '._darwinkeyboard',
-    }[_platform.system()],
-    __package__
-) # 100% coverage or bust.
+if _platform.system() == 'Windows':
+    from. import _winkeyboard as _os_keyboard
+elif _platform.system() == 'Linux':
+    from. import _nixkeyboard as _os_keyboard
+elif _platform.system() == 'Darwin':
+    from. import _darwinkeyboard as _os_keyboard
+else:
+    raise OSError("Unsupported platform '{}'".format(_platform.system()))
 
 from ._keyboard_event import KEY_DOWN, KEY_UP, KeyboardEvent, normalize_name as _normalize_name
 from ._generic import GenericListener as _GenericListener
